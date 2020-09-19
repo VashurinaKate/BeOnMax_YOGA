cda603727eef15f9a696717cdf965ddeec8e2ad7
VashurinaKate <wasturina.ekatterina@gmail.com>
2020-09-19 19:20:05 +0300

    Added slider and calc

--- js/script.js
+++ js/script.js
@@ -157,42 +157,189 @@ window.addEventListener('DOMContentLoaded', function() {
         failure: 'Что-то пошло не так...'
     };
 
-    let form = document.querySelector('.main-form'),
+    let form = document.querySelector('.main-form')[0],
+        formBottom = document.getElementById('form'),
         input = form.getElementsByTagName('input'),
         statusMessage = document.createElement('div');
 
         statusMessage.classList.add('status');
+// *метод без promise*
+    // form.addEventListener('submit', function(event) {
+    //     event.preventDefault(); // отмена стандартного поведения браузера - в данном случае при отравке формы сраница не перезагружается
+    //     form.appendChild(statusMessage);
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault(); // отмена стандартного поведения браузера - в данном случае при отравке формы сраница не перезагружается
-        form.appendChild(statusMessage);
+    //     let request = new XMLHttpRequest();
+    //     request.open('POST', 'server.php');
+    //     request.setRequestHeader ('Content-Type', 'application/json; charset=utf-8'); // настройки заголовков http запросов
 
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader ('Content-Type', 'application/json; charset=utf-8'); // настройки заголовков http запросов
+    //     let formData = new FormData(form);
 
-        let formData = new FormData(form);
+    //     let obj = {};
+    //     formData.forEach(function(value, key) {
+    //         obj[key] = value;
+    //     });// превращаем в json формат
+    //     let json = JSON.stringify(obj);
 
-        let obj = {};
-        formData.forEach(function(value, key) {
-            obj[key] = value;
-        });// превращаем в json формат
-        let json = JSON.stringify(obj);
+    //     request.send(json);
 
-        request.send(json);
+    //     request.addEventListener('readystatechange', function() {
+    //         if (request.readyState < 4) {
+    //             statusMessage.innerHTML = message.loading; // если грузится, сюда можно поместить значок загрузки
+    //         } else if (request.readyState === 4 && request.status == 200) {
+    //             statusMessage.innerHTML = message.success;
+    //         } else {statusMessage.innerHTML = message.failure;
+    //         }
+    //     });
 
-        request.addEventListener('readystatechange', function() {
-            if (request.readyState < 4) {
-                statusMessage.innerHTML = message.loading; // если грузится, сюда можно поместить значок загрузки
-            } else if (request.readyState === 4 && request.status == 200) {
-                statusMessage.innerHTML = message.success;
-            } else {statusMessage.innerHTML = message.failure;
+    //     for (let i = 0; i < input.length; i++) {
+    //         input[i].value = '';
+    //     }// очищаем инпуты
+    // });
+// *метод с promise*
+    function sendForm(elem) {
+        elem.addEventListener('submit', function(e){
+            e.preventDefault();
+                elem.appendChild(statusMessage);
+                let formData = new FormData(elem);
+
+                function postData(data) {
+                    return new Promise(function(resolve, reject){
+                        let request = new XMLHttpRequest();
+
+                        request.open('POST', 'server.php');
+
+                        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+                        request.onreadystatechange = function() {
+                            if (request.readyState < 4) {
+                                resolve()
+                            } else if (request.readyState === 4) {
+                                if (request.status == 200 && request.status < 300) {
+                                    resolve()
+                                }
+                                else {
+                                    reject()
+                                }
+                            }
+                        }
+                        request.send(data);
+                    })
+                } // End postData
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
             }
-        });
 
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = '';
-        }// очищаем инпуты
+            postData(formData)
+                .then(()=> statusMessage.innerHTML = message.loading)
+                .then(()=> {
+                    thanksModal.style.display = 'block';
+                    mainModal.style.display = 'none';
+                    statusMessage.innerHTML = '';
+                })
+                .catch(()=> statusMessage.innerHTML = message.failure)
+                .then(clearInput)
+        });
+    }
+    sendForm(form);
+    sendForm(formBottom)
+
+    // ------SLIDER------
+
+    let slideIndex = 1, //параметр текущего слайда
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        // for (let i = 0; i < slides.length; i++) {
+        //     slides[i].style.display = 'none';
+        // }
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
     });
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i =0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+                currentSlide(i);
+            }
+        }
+    });
+
+    // --------CALC-------
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+    restDays = document.querySelectorAll('.counter-block-input')[1],
+    place = document.getElementById('select'),
+    totalValue = document.getElementById('total'),
+    personsSum = 0,
+    daysSum = 0,
+    total = 0;
+
+    totalValue.innerHTML = 0; //Вписываем изначальную сумму
+
+    persons.addEventListener('input', function() {
+        personsSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0; //проверка на ввод, если оба поля пустые
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    restDays.addEventListener('input', function() {
+        
+        daysSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (persons.value == '' || restDays.value == '') {
+            totalValue.innerHTML = 0; //проверка на ввод, если оба поля пустые
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change', function() {
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            let a = total; // промежуточная переменная
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+        }
+    })
 }); // Выполнение скрипта начнется только тогда, когда прогрузятся основные элементы дерева DOM
 
